fa4dfa0f4164496f05d57b7584ed9404f960feda
Natsa <claudio.natsa@gmail.com>
2025-01-25 00:37:51 -0300

    se corrige dependencias mal instaladas de tailwind.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4994d2',
+        secondary: '#1B365D',
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
 }
